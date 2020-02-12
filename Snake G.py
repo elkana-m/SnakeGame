7e2db6f007790f2545ee759ec3ce8d58a2c17b3c
Elkana <MUNGANGA@Blues-MacBook-Air.local>
2020-02-11 23:22:45 -0600

    Initial commit

---
+++ Snake G.py
@@ -0,0 +1,12 @@
+import turtle
+sc = turtle.Screen()
+sc.title("Python Snake")
+sc.bgcolor("lightgreen")
+sc.setup (width = 500, height = 500)
+
+snake = turtle.Turtle()
+snake.color("black")
+
+snake.shape = "Turtle"
+
+sc.mainloop()
